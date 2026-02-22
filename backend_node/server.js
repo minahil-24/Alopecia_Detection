01a1bd73ac378fc85c4d9db63d12e08f7d8eabf9
minahil-24 <minahilsharif28@gmail.com>
2026-02-22 16:50:19 +0500

    Push full project

--- backend_node/server.js
+++ backend_node/server.js
@@ -7,6 +7,7 @@ const cors = require('cors');
 const jwt = require('jsonwebtoken');
 const FormData = require('form-data');
 
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 const FASTAPI_URL = process.env.FASTAPI_URL || 'http://localhost:8000';
